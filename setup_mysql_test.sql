d528e9f25ed1713782c77f445f89fe529e62b1f2
root <besckinder@gmail.com>
2023-11-18 10:48:23 -0800

    Fixed SQL queries

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- Prepares a MySQL test server for the project
 
-CREATE IF NOT EXISTS DATABASE hbnb_test_db;
-CREATE IF NOT EXISTS USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performace_schema.* TO 'hbnb_test'@'localhost';
