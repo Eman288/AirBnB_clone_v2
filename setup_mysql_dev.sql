dc5281bc29f4deb42cc58c52907265bebe280667
root <besckinder@gmail.com>
2023-11-18 12:23:58 -0800

    added comment for every query

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,13 @@
 -- Prepares a MySQL dev server for the project
 
+-- Creates the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Creates user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- grants select privileges to the created user on the performace_schema db
 GRANT SELECT ON performace_schema.* TO 'hbnb_dev'@'localhost';
+
+-- grants all privileges to the created user on the dev db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,13 @@
 -- Prepares a MySQL test server for the project
 
+-- Creates the test db if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Creates the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grants all privileges on the test db to the created user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grants select privilege to the created user on the performace_schema db
 GRANT SELECT ON performace_schema.* TO 'hbnb_test'@'localhost';
