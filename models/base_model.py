02af906548f0f62a3427189b40caa35ae5edc77e
Eman288 <emantamam107@gmail.com>
2023-11-19 05:09:35 -0800

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            '''storage.new(self)'''
+            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -41,7 +41,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-         models.storage.new(self)
+        """storage.new(self)"""
         storage.save()
 
     def to_dict(self):
@@ -57,4 +57,4 @@ class BaseModel:
 
     def delete(self):
         """delete the current instance from the storage"""
-          models.storage.delete(self)
+          storage.delete(self)
