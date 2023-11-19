193f4b08317aff2ce00cdfc238352528cb3bc4e2
Eman288 <emantamam107@gmail.com>
2023-11-19 04:45:01 -0800

    0x02-AirBnBclone- not finished task6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
