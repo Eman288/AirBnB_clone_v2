16858d38a6aaeef9801dd32af6024aa02c0bca59
root <besckinder@gmail.com>
2023-11-21 06:02:40 -0800

    Fixed syntax errors

--- models/amenity.py
+++ models/amenity.py
@@ -2,11 +2,11 @@
 """ State Module for HBNB project """
 import sqlalchemy
 from models.base_model import BaseModel, Base
-from sqlalchemny import String, Column, ForeignKey
+from sqlalchemy import String, Column, ForeignKey
 
 
-class Amenity(BaseModel), Base:
+class Amenity(BaseModel, Base):
     ''' Table for amenities '''
 
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=Flase)
+    name = Column(String(128), nullable=False)
