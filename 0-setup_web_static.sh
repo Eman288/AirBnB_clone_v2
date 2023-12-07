5a3d5c2c24099c6c509fc3a54d957c7fe5933b56
root <besckinder@gmail.com>
2023-12-07 12:21:07 -0800

    Trying something different

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,27 +3,11 @@
 
 sudo apt-get -y update > /dev/null
 sudo apt-get -y -q install nginx > /dev/null
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
+echo "This is a test" | sudo tee sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo touch /data/web_static/current/index.html
-echo "\
-<html>
-        <head>
-        </head>
-        <body>
-                Holberton School
-        </body>
-</html>" | sudo tee /data/web_static/current/index.html > /dev/null
 sudo chown -R ubuntu:ubuntu /data
-
-sudo sed -i -e '/server_name _;/{
-        a\
-        \
-        location /hbnb_static/ {\
-                alias /data/web_static/current/;\
-        }
-}' /etc/nginx/sites-available/default
+sudo sed -i'38i\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n}\n}' /etc/nginx/sites-available/default
 
 sudo service nginx start
-
