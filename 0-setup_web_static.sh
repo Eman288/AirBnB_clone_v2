9ac00ad59a9945f609503e0639a7be290ddb961e
root <besckinder@gmail.com>
2023-12-06 16:07:17 -0800

    Checker fix attempt 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# sets up my web servers for deployment
+# Sets up my web servers for deployment
 
-sudo apt-get -y -q install nginx > /dev/null
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared
+sudo apt-get -y install nginx > /dev/null
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/tests/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
+sudo chown -hR ubuntu:ubuntu /data
 
 sudo sed -i -e "/server_name _;/{a\
         \
