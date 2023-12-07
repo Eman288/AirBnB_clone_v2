af84dc9bd29cf8c71c5a8030b84fdd522fe6c73a
root <besckinder@gmail.com>
2023-12-07 13:05:51 -0800

    Removing /dev/null/

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # sets up my web servers for deployment
 
-sudo apt-get -y update > /dev/null
-sudo apt-get -y -q install nginx > /dev/null
+sudo apt-get -y update
+sudo apt-get -y -q install nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
-echo "This is a test" | sudo tee sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "Testing" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i'38i\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n}\n}' /etc/nginx/sites-available/default
