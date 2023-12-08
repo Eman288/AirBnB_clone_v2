ca54bff0c0940e673441a6b9b944a4359628e842
root <besckinder@gmail.com>
2023-12-07 16:43:55 -0800

    Please work

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,56 +2,45 @@
 
 ''' Distributes an archive to my web servers '''
 
-from fabric.api import run, put, settings
+from fabric.api import run, put, env
 from paramiko.pkey import PKey
 import os
 import sys
+
+env.hosts = ['54.146.81.93', '34.232.76.218']
 
 
 def do_deploy():
     ''' deployes the archived files to the web servers '''
 
-    env = os.environ
-    env.key_filename = "~/.ssh/school.pem"
-    env.hosts = ['54.146.81.93', '34.232.76.218']
-    env.user = 'ubuntu'
-
-    for host in env.hosts:
-        with settings(host_string=host):
-            archive_path = 'versions/web_static_20231207141839.tgz'
-            archive_name = 'web_static_20231207141839.tgz'
-            dir_name = archive_name[:-4]
-            results = []
-
-            if not(os.path.exists(archive_path)):
-                return False
-
-            # Upload files to the server
-            results.append(put(archive_path, "/tmp"))
-            results.append(run('cd /tmp'))
-
-            # Make a dir for the files to be unarchived
-            results.append(run('mkdir -p /data/web_static/releases/'
-                               + f'{dir_name}'))
-            # Unarchive files into the designates folder
-            results.append(run(f'tar -xzf /tmp/{archive_name} -C'
-                               + f' /data/web_static/releases/{dir_name}/'))
-            # Remove .tgz file
-            results.append(run(f'rm -rf /tmp/{archive_name}'))
-            # Move the static files to the desired folder
-            results.append(run(f'mv /data/web_static/releases/{dir_name}/'
-                               + f'web_static/* '
-                               + f'/data/web_static/releases/{dir_name}'))
-            # Remove emptied folder
-            results.append(run(f'rm -rf /data/web_static/releases/'
-                               + f'{dir_name}/web_static'))
-            # Removes existing symbolic link
-            results.append(run(f'rm -rf /data/web_static/current'))
-            # Create new symbolic link
-            results.append(run(f'ln -sf /data/web_static/releases/{dir_name}'
-                               + f'/data/web_static/current'))
-
-    if None in results or 0 in results:
+    if not (os.pathexists(archive_path)) is False:
         return False
-    else:
+
+    try:
+        # Upload files to the server
+        put(archive_path, "/tmp")
+        run('cd /tmp')
+
+        # Make a dir for the files to be unarchived
+        run('mkdir -p /data/web_static/releases/'
+            + f'{dir_name}')
+        # Unarchive files into the designates folder
+        run(f'tar -xzf /tmp/{archive_name} -C'
+            + f' /data/web_static/releases/{dir_name}/')
+        # Remove .tgz file
+        run(f'rm -rf /tmp/{archive_name}')
+        # Move the static files to the desired folder
+        run(f'mv /data/web_static/releases/{dir_name}/web_static/* '
+            + f'/data/web_static/releases/{dir_name}')
+        # Remove emptied folder
+        run(f'rm -rf /data/web_static/releases/'
+            + f'{dir_name}/web_static')
+        # Removes existing symbolic link
+        run(f'rm -rf /data/web_static/current')
+        # Create new symbolic link
+        run(f'ln -sf /data/web_static/releases/{dir_name}'
+            + f'/data/web_static/current')
+
         return True
+    except Exception as e:
+        return False
