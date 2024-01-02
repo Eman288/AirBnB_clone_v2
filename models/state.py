b748d5ddcb4b5dad77a9e5ca231e32296ba56c95
root <besckinder@gmail.com>
2024-01-02 14:47:08 -0800

    Sometimes you gotta step away, do some living

--- models/state.py
+++ models/state.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 from os import getenv
-from models import storage
+from models.engine.file_storage import FileStorage
 
 
 class State(BaseModel, Base):
@@ -14,14 +14,20 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        cities = relationship("City", backref="state", cascade="delete")
 
-    @property
-    def cities(self):
-        ''' Gets all cities associated with the current state '''
-        cities = []
-        for city_id, city_obj in FileStorage().all(City).items():
-            if city_obj.state_id == self.id:
-                city_list.append(city_obj)
-        return cities
+    if getenv("HBNB_TYPE_STORAGE") == 'fs':
+        @property
+        def cities(self):
+            ''' Gets all cities associated with the current state '''
+            storage = FileStorage()
+            city_list = list()
+            storage.reload()
+            all_cities = storage.all(City)
+
+            for key, obj in all_cities.items():
+                if obj.state_id == self.id:
+                    city_list.append(obj)
+            return city_list
+
+    else:
+        cities = relationship("City", backref="states", cascade="delete")
